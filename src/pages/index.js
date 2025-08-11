d5db7bb9e4e407295ce513ef8ae358f9e576824e
electricman07 <glenp@telus.net>
2025-08-11 11:24:58 -0700

    added index.js with all page links

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export { default as HomeLayout } from "./HomeLayout";
+export { default as About } from "./About";
+export { default as Contact } from "./Contact";
+export { default as Portfolio } from "./Portfolio";
+export { default as Qualifications } from "./Qualifications";
+export { default as Blog } from "./Blog";
+export { default as Landing } from "./Landing";
